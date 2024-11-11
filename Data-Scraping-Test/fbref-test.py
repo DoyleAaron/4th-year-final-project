1100ce3bec11a6f7127368f53ec52932bff39285
DoyleAaron <aarondoyler101@gmail.com>
2024-11-11 21:02:09 +0000

    Data Scraper
    I now have a working version of the data scraper working using headers in it to avoid 403 issues on the site

--- Data-Scraping-Test/fbref-test.py
+++ Data-Scraping-Test/fbref-test.py
@@ -1,8 +1,16 @@
-# libraries
 import pandas as pd
+import requests
 
-# fbref table link
-url_df = 'https://fbref.com/en/comps/Big5/gca/players/Big-5-European-Leagues-Stats#stats_gca'
+# URL and headers
+url = 'https://fbref.com/en/squads/19538871/Manchester-United-Stats#all_stats_standard'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://www.google.com/"
+}
 
-df = pd.read_html(url_df)
-print(df)
+# Make a request to the page with headers
+response = requests.get(url, headers=headers)
+df_list = pd.read_html(response.text)
+df = df_list[0]  # This is being used to select which table on the page to use
+print(df.head())
